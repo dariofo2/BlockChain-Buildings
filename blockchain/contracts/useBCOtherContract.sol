d6243fd6d33425af10b3f815503c62de9129f978
dariofo2 <alejandrodariofo2@gmail.com>
2025-02-11 00:14:35 +0100

    Done. Now ERC20 Works perfect outside Calling from Outside Contract

--- blockchain/contracts/useBCOtherContract.sol
+++ blockchain/contracts/useBCOtherContract.sol
@@ -4,26 +4,26 @@ pragma solidity ^0.8.28;
 import "./ERC20/IERC20.sol";
 
 contract BCTokenActions {
-    IERC20 _token;
+    IERC20 private _token;
     address private owner;
+    address private contractAddress;
     //Constructor IERC20 token is the Contract Address of the ERC20 Created Crypto
     constructor (address token) {
         _token = IERC20(token);
         owner = msg.sender;
+        contractAddress = address(this);
     }
 
-    function transferBC(uint value) public returns (bool isDone) {
-        address myAddress = address(bytes20(bytes("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199")));
-        //address from = msg.sender;
-        return _token.transferFrom(msg.sender,myAddress,value);
+    function transferBC(uint value) external returns (bool isTrue) {
+        address from = msg.sender;
+        return _token.transferFrom(from,contractAddress,value);
     }
 
     function test() public view returns (IERC20 token, address own,address sender) {
         return (_token,owner,msg.sender);
     }
     function withdrawBC() external {
-        address to = msg.sender;
-        _token.transferFrom(address(this), to, 500);
+        _token.transfer(owner,_token.balanceOf(contractAddress));
     }
 
     function checkBalanceBC() public view returns (uint _amount) {
--- frontend/InteractWithBCCrypto.js
+++ frontend/InteractWithBCCrypto.js
@@ -2,80 +2,76 @@ import Web3 from "web3";
 import fs from "fs";
 import deployAndSendUploadContract from "./deployAndSendUploadContract.js";
 
-const web3=new Web3("http://127.0.0.1:8545");
-const block=web3.eth.getBlock;
-const compiledContractRead=fs.readFileSync("blockchain/artifacts/contracts/useBCOtherContract.sol/BCTokenActions.json");
-const compiledContract=JSON.parse(compiledContractRead);
+const web3 = new Web3("http://127.0.0.1:8545");
+const block = web3.eth.getBlock();
+const compiledContractRead = fs.readFileSync("blockchain/artifacts/contracts/useBCOtherContract.sol/BCTokenActions.json");
+const compiledContract = JSON.parse(compiledContractRead);
 
 //El ABI del contrato
 //El contract Address Es distinto cada vez que lo subes. Hay que cambiarlo.
-const abi=compiledContract.abi;
-const contractAddress="0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+const abi = compiledContract.abi;
+const contractAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+
+//EL ABI del contrato ERC20
+let readCompiledContracterc = fs.readFileSync("./blockchain/artifacts/contracts/BC.sol/ERC20BlockchainSupply.json", "utf-8");
+const ERC20JsonCompiled = JSON.parse(readCompiledContracterc);
+const abiERC20 = ERC20JsonCompiled.abi;
+const contractERC20Address="0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
 
 //Coger la cuenta que vamos a usar para llamar al contrato
-const privatekey="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+const privatekey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 const account = web3.eth.accounts.privateKeyToAccount(privatekey);
 web3.eth.wallet.add(account);
-
 //web3.eth.deffaultaccount sets the address account that 
 // default is used on Everything (calls, send and balanceof,transfer...etc)
-web3.eth.defaultAccount=account.address;
+web3.eth.defaultAccount = account.address;
 console.log(web3.eth.defaultAccount);
 
-let contract=new web3.eth.Contract(abi,contractAddress,{from:account.address});
+//Inicializar CONTRATOS!!
+let contract = new web3.eth.Contract(abi, contractAddress, { from: account.address });
+const contractERC20 = new web3.eth.Contract(abiERC20, contractERC20Address, {from:account.address});
 
-async function callContractBalanceBC () {
-    let balance=await contract.methods.checkBalanceBC().call()
-    
+
+
+
+async function callContractBalanceBC() {
+    let balance = await contract.methods.checkBalanceBC().call()
+
     console.log(balance);
 }
+async function sendContractERC20transferBC() {
+    let transfer = await contractERC20.methods.transfer("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", 100).send({ from: account.address });
+}
+async function sendContractTransferBC(value) {
+    let transferAprovalSpender=await contractERC20.methods.approve(contractAddress,value).send({from:account.address});
+    let transferEncondedAbi = await contract.methods.transferBC(value).send({ from: account.address, gas: 500000 });
+    console.log(transferEncondedAbi)
 
-async function sendContractTransferBC () {
-    
-    try {
-        let amount = web3.utils.fromWei("100000000000000000000000","ether");
-        let transferEncondedAbi=await contract.methods.transferBC(amount).encodeABI();
-        console.log(transferEncondedAbi);
-        let tx={
-            
-            from:account.address,
-            to:contractAddress,
-            gas:50000000000,
-            maxPriorityFeePerGas:100000000,
-            maxFeePerGas: 1000000000,
-            data: transferEncondedAbi
-        }
-        
-        var signed=await web3.eth.accounts.signTransaction(tx,privatekey);
-        
-        let rawtransaction=await signed.rawTransaction;
-        let final=await web3.eth.sendSignedTransaction(rawtransaction);
-        //console.log(final);
-        
-    } catch (error) {
-        console.error(error);
-    }
-    
+
+
 }
 
-async function sendContractwithDrawBC () {
-    let resp=await contract.methods.withdrawBC().send();
+async function sendContractwithDrawBC() {
+    let resp = await contract.methods.withdrawBC().send();
 }
 
-async function contractTest () {
-    let resp=await contract.methods.test().call();
+async function contractTest() {
+    let resp = await contract.methods.test().call();
     console.log(resp);
 }
-async function checkEthereumBalance () {
-    let balancee=await web3.eth.getBalance(account.address)
-    balancee=web3.utils.fromWei(balancee.toString(),"ether");
+async function checkEthereumBalance() {
+    let balancee = await web3.eth.getBalance(account.address)
+    balancee = web3.utils.fromWei(balancee.toString(), "ether");
     console.log(balancee)
 }
 
-sendContractTransferBC();
+//sendContractERC20transferBC();
+//sendContractTransferBC(1000);
 //sendContractwithDrawBC();
 //contractTest();
 callContractBalanceBC();
+checkEthereumBalance();
 
 
 
